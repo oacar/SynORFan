04f97f5f5be330d478685e9875052648d2d94271
Omer Acar <omeracar0@gmail.com>
2020-03-09 15:07:02 -0400

    moved scripts to folders

--- bash_scripts/run_analysis_rev.sh
+++ bash_scripts/run_analysis_rev.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-for i in *;
+#for i in *;
+ls | tac | while read i;
 do
 name="$(cut -d'_' -f1 <<<$i)";
 #muscle_file_name = ""
-if ls $i/*subalignment* 1> /dev/null 2>&1;
+if ls $i/*muscle* 1> /dev/null 2>&1;
 then
 #    echo "file exists"
-#    python ../../../bioconductor.py -p $i -n $name -y ../../orf_genomic_all.fasta -m
-    continue
+    python ../../../bioconductor.py -p $i -n $name -y ../../orf_genomic_all.fasta -m
 else
     python ../../../bioconductor.py -p $i -n $name -y ../../orf_genomic_all.fasta -ap
 fi
-
 #python ../../analysis.py -p $i -n $name
 done;
