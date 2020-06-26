813f7f8830bfef129652629a2018f57829c40a8f
Omer Acar <omeracar0@gmail.com>
2020-06-26 10:40:21 -0400

    removed unrelated files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='synorfan',
+    packages=find_packages(),
+    version='0.1.0',
+    description='',
+    author='Omer Acar',
+    license='MIT',
+)
